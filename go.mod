779ae9a0c61229ab6bebaa3014e27d60c493ac08
Christian Cavuti <christian.cavuti@gmail.com>
2024-05-13 17:28:28 +0200

    refactor: fresh start

--- go.mod
+++ go.mod
@@ -1,15 +1,3 @@
 module github.com/Kavuti/go-service-utils
 
-go 1.19
-
-require (
-	github.com/ajg/form v1.5.1 // indirect
-	github.com/go-chi/render v1.0.2 // indirect
-	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.11.2 // indirect
-	github.com/leodido/go-urn v1.2.1 // indirect
-	golang.org/x/crypto v0.5.0 // indirect
-	golang.org/x/sys v0.4.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
-)
+go 1.21.7
---
+++ tmpl/tmpl.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strconv"
+	"strings"
+	"text/template"
+)
+
+func main() {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		panic(errors.New("unable to get the current filename"))
+	}
+	directory := filepath.Dir(filename)
+
+	projectDirectory, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	entityName := flag.String("name", "Foo", "The name of the entity to create")
+	projectName := flag.String("project-name", filepath.Base(projectDirectory), "The project name")
+	flag.Parse()
+
+	// Protobuf template
+	renderTemplate(directory, projectDirectory+"/proto", *entityName, *projectName, "proto.tmpl", strings.ToLower(*entityName), "proto")
+
+	// Service template
+	renderTemplate(directory, projectDirectory+"/service", *entityName, *projectName, "service.tmpl", strings.ToLower(*entityName), "go")
+
+	// SQLC queries template
+	renderTemplate(directory, projectDirectory+"/db/queries", *entityName, *projectName, "sqlc_queries.tmpl", strings.ToLower(*entityName), "sql")
+
+	// Migration
+	r, _ := regexp.Compile("[0-9]{4}")
+
+	maxNumber := 0
+	filepath.Walk(projectDirectory+"/db/migrations", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if r.MatchString(info.Name()) {
+			number, _ := strconv.Atoi(r.FindString(info.Name()))
+			if number > maxNumber {
+				maxNumber = number
+			}
+		}
+		return nil
+	})
+	renderTemplate(directory, projectDirectory+"/db/migrations", *entityName, *projectName, "migration.tmpl", fmt.Sprintf("%04d_%s", maxNumber+1, strings.ToLower(*entityName)), "sql")
+}
+
+func renderTemplate(directory string, destinationDirectory string, entityName string, projectName string, templateName string, filename string, fileext string) {
+	var tmplData struct {
+		EntityName  string
+		ProjectName string
+	}
+	tmplData.EntityName = entityName
+	tmplData.ProjectName = projectName
+
+	funcMap := template.FuncMap{
+		"ToLower": strings.ToLower,
+	}
+
+	tmpl, err := template.New(templateName).Funcs(funcMap).ParseFiles(directory + "/" + templateName)
+	if err != nil {
+		panic(err)
+	}
+	buf := &bytes.Buffer{}
+	err = tmpl.Execute(buf, tmplData)
+	if err != nil {
+		panic(err)
+	}
+	os.WriteFile(destinationDirectory+"/"+filename+"."+fileext, buf.Bytes(), 0644)
+}
